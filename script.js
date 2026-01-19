979e0215de97e61a6c21061e554fbfa1e82e3a0c
Vasilsa1109 <arina200011@icloud.com>
2026-01-19 19:37:31 +0000

    ;,

---
+++ script.js
@@ -0,0 +1,53 @@
+let test = 'Test';
+console.log('test:', test);
+
+let value = 42;
+console.log(typeof value);
+
+let a = "10";
+let b = 10;
+console.log(typeof a);
+console.log(typeof b);
+
+let x = 10;
+let y = 20;
+let xy = (x+y)/2;
+console.log(xy);
+
+let n = 13;
+let ost = 13%5;
+console.log(ost);
+
+let count = 4;
+let sum = count + 14;
+console.log(sum);
+
+let firstname = "Anna";
+let lastname = "Ivanova";
+console.log(firstname + " " + lastname);
+
+let text = "javascript";
+console.log(text,length);
+
+let city = "London";
+console.log(city.toUpperCase);
+
+let xx = "5";
+let yy = 5;
+console.log(xx == yy);
+console.log(xx === yy);
+
+let number = -3;
+if (number >= 0) {
+    console.log("оложительное");
+}
+if (number <= 0) {
+    console.log("отрицательное");
+}
+
+let price = 1000;
+let precent = 15;
+console.log(price * precent/1);
+
+
+
