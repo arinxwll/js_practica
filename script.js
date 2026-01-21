b5f8c8205fff8493c2f491edb7345c2fa0d3328f
Vasilsa1109 <arina200011@icloud.com>
2026-01-21 20:11:44 +0000

    task

--- script.js
+++ script.js
@@ -57,6 +57,108 @@ const min = 4;
 filterLongWords(animals,min);
 
 
+const sumNumber = (arr) => {
+    let sum = 0;
+    for (const item of arr) {
+        if (typeof item === 'number') {
+            sum += item;
+        }
+    }
+    console.log(sum);
+};
+const aa = [1,"2", 3, null, 10];
+sumNumber(aa);
 
 
+function calculate(a, b, op) {
+    switch(op) {
+        case '+':
+            return a + b;
+            case '-':
+                return a - b;
+                case '*':
+                    return a * b;
+                    if (b === 0) {
+                      return"ошибка. деление на ноль";
+                    }
+            
+    }
+}
+console.log(calculate(10, 5, '+'));
 
+
+function formatUsers(users, formatter) {
+    return users.map(formatter);
+  }
+  const usersData = [
+    { name: 'Arina', age: 14 },
+    { name: 'Sonya', age: 15 },
+  ];
+  const userFormatter = (u) => `${u.name} (${u.age})`;
+  const formattUsers = formatUsers(usersData, userFormatter);
+  console.log(formattUsers);
+  const anotherFormatter = (user) => ` ${user.name},${user.age}`;
+  const formattUsers2 = formatUsers(usersData, anotherFormatter);
+  console.log(formattUsers2);
+
+
+  function greet(name, hour) {
+    let greeting;
+    if (hour >= 6 && hour <= 11) {
+      greeting = "good morning";
+    } else if (hour >= 12 && hour <= 17) {
+      greeting = "good afternoon";
+    } else if (hour >= 18 && hour <= 22) {
+      greeting = "good ";
+    } else {
+      greeting = "good night";
+    }
+    return `${greeting}, ${name}!`;
+  }
+  console.log(greet("arina", 7));
+  console.log(greet("arina", 15));
+  console.log(greet("arina", 18));
+
+  const isEmpty = (arr) => {
+    return arr.length === 0;
+  };
+  console.log(isEmpty([]));
+  console.log(isEmpty([1]));
+  console.log(isEmpty([]));
+  console.log(isEmpty(["a"]));
+  console.log(isEmpty(["null"]))
+
+
+  const formatName = (name) => {
+    if (typeof name !== 'string' || name.length === 0) {
+      return "";
+    }
+    const casename = name.toLowerCase();
+    const first = casename.charAt(0).toUpperCase();
+    const restOfName = casename.slice(1);
+    return first + restOfName;
+  };
+  console.log(formatName("alex"));
+  console.log(formatName("alEX"));
+  console.log(formatName(""));
+  console.log(formatName("a"));
+
+  const isEmail = (email) => {
+    if (typeof email !== 'string') {
+      return false;
+    }
+    return email.includes('@');
+  };
+  console.log(isEmail("ghytvf@example.com"));
+  console.log(isEmail("user.name@hjbiloh.ru"));
+  console.log(isEmail("gyhugyugt.com"));
+  console.log(isEmail("vgfcfj@example"));
+
+  const createWallet = () => {
+    let balance = 0;
+  
+    return (amount) => {
+      balance += amount;
+      return balance;
+    };
+  };
